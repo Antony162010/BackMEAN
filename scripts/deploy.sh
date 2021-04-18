f809749e105490c240ba546d6e29b82832eeba28
Antony Montalvo <antonis162010@gmail.com>
2021-04-18 03:11:39 -0500

    Fourth test 6

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [[ -z "${MONGODB_URI}" ]]; then
-    MONGODB_URI="cat mynewfile"
+    MONGODB_URI="$(cat mynewfile)"
 else
     MONGODB_URI="${MONGODB_URI}"
 fi
 
 echo $MONGODB_URI
-# pm2-runtime /app/ecosystem.config.js
+pm2-runtime /app/ecosystem.config.js
