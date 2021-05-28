473dfb01a6e284808c00a0d72a2ff24a6ff9e934
Antony Montalvo <antonis162010@gmail.com>
2021-05-28 15:52:17 -0500

    Test pub-sub and cloud-function 2

--- src/controllers/apis/extern.controller.js
+++ src/controllers/apis/extern.controller.js
@@ -8,13 +8,13 @@ const pubSub = new PubSub({
     projectId: process.env.GC_PROJECT_ID,
 });
 
-const ExternController = {},;
+const ExternController = {};
 
 ExternController.sendMessage = async (req, res) => {
     try {
         const report = req.body;
 
-        const buffer = Buffer.from(JSON.stringify(report))
+        const buffer = Buffer.from(JSON.stringify(report));
         await pubSub.topic(process.env.GC_PUBSUB_TOPIC).publish(buffer);
 
         res.status(200).json({status: "correct"});
