651c5113e05e1a9640ea5fb7e69afa422bcea86e
Antony Montalvo <antonis162010@gmail.com>
2021-05-30 01:20:29 -0500

    Fix pubsub 15

--- scripts/build-container-local.sh
+++ scripts/build-container-local.sh
@@ -1,6 +1,7 @@
 GC_PROJECT_ID=$(grep GC_PROJECT_ID .env.prod | cut -d '=' -f2)
 export CI_COMMIT_SHA=$(git rev-parse --verify HEAD)
 export CONTAINER_REGISTRY=us.gcr.io/$GC_PROJECT_ID/backmean
+export TYPE_ENV=.prod
 
 # docker login -u _json_key --password-stdin https://us.gcr.io < src/config/gcp-key.json # Only first time
 envsubst < ./system/template-docker-compose.yml > ./docker-compose.yml
--- scripts/build-container-prod.sh
+++ scripts/build-container-prod.sh
@@ -1,3 +1,5 @@
+export TYPE_ENV=.prod
+
 envsubst < ./system/template-docker-compose.yml > ./docker-compose.yml
 docker-compose --env-file ./.env.prod config > ./docker-compose.yaml
 rm ./docker-compose.yml
