20585c74dbc99f07a48eeec13225e1d5d6fab7f7
Antony Montalvo <antonis162010@gmail.com>
2021-05-30 01:06:50 -0500

    Fix pubsub 12

--- scripts/build-container-prod.sh
+++ scripts/build-container-prod.sh
@@ -1,8 +1,6 @@
-export TYPE_ENV=.prod
-
 envsubst < ./system/template-docker-compose.yml > ./docker-compose.yml
-docker-compose --env-file ./.env.prod config > docker-compose.yaml
-rm docker-compose.yml
-mv docker-compose.yaml docker-compose.yml
+docker-compose --env-file ./.env.prod config > ./docker-compose.yaml
+rm ./docker-compose.yml
+mv ./docker-compose.yaml ./docker-compose.yml
 docker-compose build 
 docker-compose push
